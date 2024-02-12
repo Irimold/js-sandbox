806e8404eccbd6a92c0fd49fe22724328154b3d0
Levine Lighto <lighto.cc2@gmail.com>
2024-02-12 15:19:42 +0800

    check if script actually enter the listener

--- speech-synthesis/available-voices/index.js
+++ speech-synthesis/available-voices/index.js
@@ -7,6 +7,8 @@ trigger.addEventListener('click', () => {
     
     const voices = speechSynthesis.getVoices().filter(voice => voice.lang == 'id-ID')
     voices.forEach(voice => {
+
+        console.log(voice)
         
         const listElem = document.createElement('li')
         listElem.innerHTML = `${voice.name} (URI: ${voice.voiceURI}) (Lang: ${voice.lang})`
