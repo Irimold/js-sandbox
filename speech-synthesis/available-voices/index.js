e9398b717e997984ad2c85e5685bb430276c6674
Levine Lighto <lighto.cc2@gmail.com>
2024-02-12 15:15:35 +0800

    Check available voice

---
+++ speech-synthesis/available-voices/index.js
@@ -0,0 +1,18 @@
+const trigger = document.querySelector('#trigger')
+const listContainer = document.querySelector('#container')
+
+trigger.addEventListener('click', () => {
+
+    listContainer.innerHTML = ''
+    
+    const voices = speechSynthesis.getVoices().filter(voice => voice.lang == 'id-ID')
+    voices.forEach(voice => {
+        
+        const listElem = document.createElement('li')
+        listElem.innerHTML = `${voice.name} (URI: ${voice.voiceURI}) (Lang: ${voice.lang})`
+
+        listContainer.appendChild(listElem)
+
+    })
+
+})
